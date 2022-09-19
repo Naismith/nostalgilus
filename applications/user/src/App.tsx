2b8769452fafea7b98934227dcb75df8a690f2e8
Chris Naismith <cnaismith@hopper.com>
2022-09-18 21:15:23 -0400

    ability to render forums on home page

--- applications/user/src/App.tsx
+++ applications/user/src/App.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+const Forum = ({ forum }: { forum: any }) => (
+  <div>
+    <p>{forum.name}</p>
+    <ul>
+      {forum.children.map((childForum: any) => (
+        <li key={childForum.id}>{childForum.name}</li>
+      ))}
+    </ul>
+  </div>
+);
 
 const App = () => {
-  return <div>Hello World</div>;
+  const [forums, setForums] = useState<any[]>([]);
+
+  useEffect(() => {
+    const main = async () => {
+      const response = await fetch("/api/v0/forums/");
+      const data = await response.json();
+      setForums(data.forums);
+    };
+
+    main();
+  }, []);
+
+  return (
+    <div>
+      {forums.map((forum) => (
+        <Forum key={forum.id} forum={forum} />
+      ))}
+    </div>
+  );
 };
 
 export default App;
--- applications/user/src/__fixtures__/forumResponse.ts
+++ applications/user/src/__fixtures__/forumResponse.ts
@@ -1,1 +1,75 @@
-export const forumResponse = {};
+/*
+1441f677-0571-4346-b4ad-4bb8afbb3f28
+c0c522f9-7cfe-456c-a6ba-76feb4b41ccd
+8e96e597-43dc-4661-89dc-828e2e297cb6
+*/
+
+const lastPost = {
+  id: "",
+  title: "",
+  date: "", // ISO
+  user: {
+    id: "",
+    name: "",
+  },
+};
+
+export const forumResponse = {
+  forums: [
+    {
+      id: "5e7a4d1e-008d-4bfc-bb34-afd6bdcc7877",
+      name: "Main Forum",
+      description: "",
+      threads: 10,
+      posts: 100,
+      lastPost: {
+        ...lastPost,
+      },
+      children: [
+        {
+          id: "88d3bfee-089c-4650-b190-a782eeb05c55",
+          name: "Main Forum Child 1",
+          description: "",
+          lastPost: {
+            ...lastPost,
+          },
+        },
+        {
+          id: "909702dd-ccb4-4419-9f71-a821ba2be241",
+          name: "Main Forum Child 2",
+          description: "",
+          lastPost: {
+            ...lastPost,
+          },
+        },
+        {
+          id: "48e421fb-a747-495b-b396-40140566f64f",
+          name: "Main Forum Child 3",
+          description: "",
+          lastPost: {
+            ...lastPost,
+          },
+        },
+      ],
+    },
+    {
+      id: "7e16a2da-0265-42fa-8b7d-2928d3483176",
+      name: "Secondary Forum",
+      description: "",
+      threads: 10,
+      posts: 100,
+      children: [
+        {
+          id: "c0cdee83-fc11-4329-9209-8e6fbfd0db6f",
+          name: "Secondary Forum Child 1",
+          description: "",
+        },
+        {
+          id: "1b5c1bbb-83a6-4730-bac6-34bcd96153e7",
+          name: "Secondary Forum Child 2",
+          description: "",
+        },
+      ],
+    },
+  ],
+};
